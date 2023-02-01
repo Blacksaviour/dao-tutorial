cf5b39b0853d1241b77ffe2d7340e515bcb021c5
tela jude <tomszy91io@gmail.com>
2023-01-31 18:42:23 -0800

    done

---
+++ backend/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xA5DAe290232f730c875Dc8F20cE2679AF65e703B";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
